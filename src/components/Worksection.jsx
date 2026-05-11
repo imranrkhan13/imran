02bf93f8e645b40311a12655483101af47891e08
imranrkhan13 <muhammadimrank034@gmail.com>
2026-05-11 12:24:38 +0530

    project card

--- src/components/Worksection.jsx
+++ src/components/Worksection.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
-import { ProjectCard, Modal } from "./ProjectCard";
+import { ProjectCard, Modal } from "./Projectcard";
 import { projects } from "../data";
 
 const containerVariants = {
